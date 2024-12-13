7439edc2d9dcd3936609935759fdd4d57fb427ce
Rakibulislam-emon <rakibulislamemon60>
2024-12-14 02:28:34 +0600

    updated

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,6 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals"),
   {
-    ignores: [".next/**"], // Exclude `.next` directory
+    extends: ["next/core-web-vitals"], // Use the recommended Next.js config
   },
 ];
 
